b991f02eeb2aa98e6ecead6445bea20bafc8ac1d
Nik <novakn@mcmaster.ca>
2024-05-03 22:10:56 -0400

    add withsvgr

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -5,3 +5,11 @@ interface ServerSideComponentProp<
     params: Params;
     searchParams: SearchParams;
 }
+
+declare module '*.svg' {
+    import { FC, SVGProps } from 'react';
+    export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  
+    const src: string;
+    export default src;
+  }
